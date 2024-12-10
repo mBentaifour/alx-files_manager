75261796c56708bc8fc50b1b52bacb7b485f3eb8
mbentaifour <bentaifourmoh@gmail.com>
2024-12-10 13:10:33 -0800

    files manager

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,52 +1,36 @@
-/* eslint-disable import/no-named-as-default */
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-export default class AppController {
+/**
+ * Controller handling App related operations
+ */
+class AppController {
+  /**
+   * Gets the status of Redis and DB connections
+   * @param {Request} req Express request object
+   * @param {Response} res Express response object
+   */
   static getStatus(req, res) {
-    res.status(200).json({
+    const status = {
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-    });
+    };
+    res.status(200).json(status);
   }
 
-  static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+  /**
+   * Gets statistics about the number of users and files
+   * @param {Request} req Express request object
+   * @param {Response} res Express response object
+   */
+  static async getStats(req, res) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).json(stats);
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default AppController;
 

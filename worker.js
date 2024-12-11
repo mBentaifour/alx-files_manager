3df28de5009165b69ab688de711f3aa06f40d6fa
mbentaifour <bentaifourmoh@gmail.com>
2024-12-11 15:59:51 -0800

    files manager

--- worker.js
+++ worker.js
@@ -1,42 +1,33 @@
-const Queue = require('bull');
-const imageThumbnail = require('image-thumbnail');
-const fs = require('fs').promises;
-const path = require('path');
-const dbClient = require('./utils/db');
+import Queue from 'bull';
+import imageThumbnail from 'image-thumbnail';
+import fs from 'fs';
+import { ObjectId } from 'mongodb';
+import dbClient from './utils/db';
 
 const fileQueue = new Queue('fileQueue');
 
+const generateThumbnail = async (width, localPath) => {
+  const thumbnail = await imageThumbnail(localPath, { width });
+  const thumbnailPath = `${localPath}_${width}`;
+  await fs.promises.writeFile(thumbnailPath, thumbnail);
+};
+
 fileQueue.process(async (job) => {
   const { fileId, userId } = job.data;
-
-  // Vérification des paramètres
   if (!fileId) throw new Error('Missing fileId');
   if (!userId) throw new Error('Missing userId');
 
-  // Récupérer le fichier depuis la base de données
-  const file = await dbClient.db.collection('files').findOne({ _id: fileId, userId });
+  const file = await dbClient.db.collection('files').findOne({
+    _id: ObjectId(fileId),
+    userId: ObjectId(userId),
+  });
 
   if (!file) throw new Error('File not found');
-  if (file.type !== 'image') return; // Process uniquement les images
 
-  const filePath = path.join('/tmp/files_manager/', file.localPath);
-
-  // Vérifier si le fichier existe
-  try {
-    await fs.access(filePath);
-  } catch {
-    throw new Error('File not found on disk');
-  }
-
-  // Générer les vignettes
-  const sizes = [100, 250, 500];
-  for (const size of sizes) {
-    const options = { width: size };
-    const thumbnail = await imageThumbnail(filePath, options);
-    const thumbPath = `${filePath}_${size}`;
-    await fs.writeFile(thumbPath, thumbnail);
-  }
+  const sizes = [500, 250, 100];
+  const thumbnailPromises = sizes.map((size) => generateThumbnail(size, file.localPath));
+  await Promise.all(thumbnailPromises);
 });
 
-console.log('Worker started and waiting for jobs...');
+export default fileQueue;
 
